f1423492a636d594feb0a060dcbd5ee82c99e4c4
biocoopenvexin <ludovic@biocoopenvexin.fr>
2018-08-21 11:23:42 +0200

    add all

---
+++ app.js
@@ -0,0 +1,65 @@
+// Simulate config options from your production environment by
+// customising the .env file in your project's root folder.
+require('dotenv').config();
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var app = express();
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+
+//CORS Middleware
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, contentType,Content-Type, Accept, Authorization");
+    next();
+});
+
+var executeQuery = function (res, query) {
+
+    var sql = require('mssql/msnodesqlv8');
+    var config = {
+      driver: 'msnodesqlv8',
+      connectionString: process.env.MSSQLCONNECTION,
+    };
+    const pool = new sql.ConnectionPool(config).connect().then(pool => {
+        return pool.request().query(query)
+        }).then(result => {
+          let rows = result.recordset
+          res.status(200).json(rows);
+          sql.close();
+        }).catch(err => {
+          res.status(500).send({ message: `${err}`})
+          sql.close();
+        });
+}
+
+//GET API
+app.get("/api/adherents", function(req , res){
+                var query = "select * from dbo.ADHANC";
+                executeQuery (res, query);
+});
+
+app.get("/api/catalogue", function(req , res){
+                var query = "select * from dbo.CATALOGUE_COMPLET";
+                executeQuery (res, query);
+});
+
+app.get("/api/fournisseurs", function(req , res){
+                var query = "select * from dbo.FOURNIS";
+                executeQuery (res, query);
+});
+
+
+// Mongo Connection
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGOLAB_URI, { useNewUrlParser: true });
+
+
+var server = app.listen(5000, function () {
+    console.log('Server is running..');
+});
---
+++ models/fournisseurs.js
@@ -0,0 +1,61 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var FournisseurSchema = new Schema(
+  {
+    CODE_FO: {type: Number, required: true},
+  	NOM_FO: {type: String},
+  	RUE_FO: {type: String},
+  	RUE2_FO: {type: String},
+  	CPOST_FO: {type: String},
+  	VILLE_FO: {type: String},
+  	TEL_FO: {type: String},
+  	FAX_FO: {type: String},
+  	MEL_FO: {type: String},
+  	CONTACT_FO: {type: String},
+  	CLASSE_FO: {type: String},
+  	FRANCO_FO: {type: Number},
+  	FRANCOP_FO: {type: Number},
+  	FRPOIDS_FO: {type: Boolean},
+  	PORT_FO: {type: Number},
+  	DISTAN_FO: {type: Number},
+  	PRODUIT_FO: {type: String},
+  	NUMCLI_FO: {type: String},
+  	COLIS_FO: {type: Boolean},
+  	ORIBAR_FO: {type: Boolean},
+  	CALINDI_FO: {type: Boolean},
+  	CALBJP_FO: {type: Boolean},
+  	COMMEN1_FO: {type: String},
+  	COMMEN2_FO: {type: String},
+  	CORRESP_FO: {type: String},
+  	MARQUE_FO: {type: String},
+  	MODELBC_FO: {type: String},
+  	FICBAL_FO: {type: String},
+  	PPNBL_FO: {type: Number},
+  	RMPLBL_FO: {type: Number},
+  	MELBC_FO: {type: String},
+  	DATMOD_FO: {type: Date},
+  	DELIVR_FO: {type: Number},
+  	DURLIVR_FO: {type: Number},
+  	DATLIV_FO: {type: Date},
+  	EXP_BLOCKED: {type: Boolean},
+  	SYNC_DATE: {type: Date},
+  	SYNC_FROM: {type: Number},
+  	SYNC_BLOCKED: {type: Boolean},
+  	MODIF_DATE: {type: Date, default: Date.now, required: true},
+  	MODIF_BY: {type: String},
+  	CREAT_DATE: {type: Date, default: Date.now, required: true},
+  	CREAT_BY: {type: String},
+  }
+);
+
+// Virtual for Fournisseur's URL
+FournisseurSchema
+.virtual('url')
+.get(function () {
+  return '/fournisseur/' + this._id;
+});
+
+//Export model
+module.exports = mongoose.model('Fournisseur', FournisseurSchema);
