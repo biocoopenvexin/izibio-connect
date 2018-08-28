a883e086e4e42a51914effd1edddb830403e1798
biocoopenvexin <ludovic@biocoopenvexin.fr>
2018-08-28 17:37:49 +0200

    Création modèle Update

---
+++ models/update.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+// Modèle qui retrace toutes les mises à jour des différentes collections. Un seul document par collection, qui est MàJ au fur et à mesure
+
+var Schema = mongoose.Schema;
+
+var UpdateSchema = new Schema(
+  {
+    DATE_UP: {type: Date},
+  	BASE_UP: {type: String},
+  }
+);
+
+// Virtual for Update's URL
+UpdateSchema
+.virtual('url')
+.get(function () {
+  return '/updates/' + this._id;
+});
+
+//Export model
+module.exports = mongoose.model('Update', UpdateSchema);
