0fb673b5c662b0feaaed8887e9ea713ad27d51e6
biocoopenvexin <ludovic@biocoopenvexin.fr>
2018-09-04 16:23:06 +0200

    modèle rayons

---
+++ models/rayon.js
@@ -0,0 +1,22 @@
+// RAYON : liste des rayons du magasin
+
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var RayonSchema = new Schema(
+  {
+    RAYON_PR: {type: String},
+  	LIBEL_RA: {type: String},
+  }
+);
+
+// Virtual for Rayon's URL
+RayonSchema
+.virtual('url')
+.get(function () {
+  return '/rayon/' + this._id;
+});
+
+//Export model
+module.exports = mongoose.model('Rayon', RayonSchema);
