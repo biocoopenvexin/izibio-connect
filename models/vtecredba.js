adf91ef200b3db3400a2a07004ad63c423687260
biocoopenvexin <ludovic@biocoopenvexin.fr>
2018-09-04 18:00:37 +0200

    modèle vtecredba

---
+++ models/vtecredba.js
@@ -0,0 +1,23 @@
+// UPDATE : traçage des mises à jour depuis MSSQL.
+// Un seul document par collection, dont la date est MàJ au fur et à mesure
+
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var UpdateSchema = new Schema(
+  {
+    DATE_UP: {type: Date, default: Date.now, required: true},
+  	BASE_UP: {type: String},
+  }
+);
+
+// Virtual for Update's URL
+UpdateSchema
+.virtual('url')
+.get(function () {
+  return '/update/' + this._id;
+});
+
+//Export model
+module.exports = mongoose.model('Update', UpdateSchema);
